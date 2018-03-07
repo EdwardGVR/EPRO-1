a69a9471d1484add1258bdb25cc8b5ad58932a15
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-03-06 22:23:41 -0600

    Proyectos (sin proyectos)

--- views/proyectos.php
+++ views/proyectos.php
@@ -6,22 +6,44 @@
     <title>Proyectos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <script src="main.js"></script>
 </head>
 <body>
-    <header>
-        <bar>
-            asd
-        </bar>
-    </header>
     <aside >
-
+        <div class="userInfo">
+            <div class="userImg">
+                <i class="fas fa-user-circle fa-4x"></i>
+            </div>
+            <div class="userInfoDet">
+                <span>{{ username }}</span>
+                <span>{{ Rol desempe&ntilde;ado }}</span>
+                <span>Cuenta</span>
+            </div>
+        </div>
+        <hr>
+        <div class="options">
+            <button>Opci&oacute;n</button>
+            <button>Opci&oacute;n</button>
+            <button>Opci&oacute;n</button>
+            <button>Opci&oacute;n</button>
+        </div>
     </aside>
-    <section>
-        <article>
-
-        </article>
-    </section>
+    <main>
+        <header>
+            <bar>
+                Proyectos
+            </bar>
+        </header>
+        <section class="proyectos">
+            <article class="noProjects">
+                    <span>No hay proyectos</span>
+                    <div class="addFirstProject">
+                        <i class="fas fa-plus-circle fa-7x"></i>
+                    </div>
+            </article>
+        </section>
+    </main>
 
 </body>
 </html>
