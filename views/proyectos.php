08d17cfad3d41d1653623ae7f84f66ca29fd3909
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-03-06 18:54:05 -0600

    Archivos de vista de proyecto

---
+++ views/proyectos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Proyectos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <header>
+        <bar>
+            asd
+        </bar>
+    </header>
+    <aside >
+
+    </aside>
+    <section>
+        <article>
+
+        </article>
+    </section>
+
+</body>
+</html>
